98c22dd5c4a57ee2b6e7f39e65809d7fa4c43040
AayushMandavya <70195769+AayushMandavya@users.noreply.github.com>
2022-08-09 22:35:52 +0545

    Add files via upload

---
+++ First PHP.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en ">
+<head>
+    <title>PHP First</title>
+</head>
+<body>
+    <h1>First Lesson on PHP Assignment </h1>
+    <h2>By Aayush Mandavya</h2>
+    <?php
+
+        echo "My First PHP page";
+        echo "Myself Aayush Mandavya";
+
+    ?>
+</body>
+</html>
